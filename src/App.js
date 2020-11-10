18b6fb973b416f9276291565b18999afcbfc09be
Kapil Padaliya <39192484+padaliya-Kapil@users.noreply.github.com>
2020-11-10 14:19:03 -0500

    Sorting implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+//To implement type of sorting you want remove comments in Const birds.
+// Please do not forget to comment other two return types
+
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { incrementBird, addBird } from './store/birds/birds'
+import './App.css'
 
 function App() {
+  const [birdName, setBird] = useState('')
+  const birds = [...useSelector((state) => state.birds)].sort((a, b) => {
+    // remove comments below  to do Sorting by name (a)
+    return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1
+
+    // remove comments below  to do  Sorting by view count descending(b)
+    // return a.views > b.views ? 1 : -1
+
+    //  remove comments below  to do Sorting by view count ascending(c)
+    // return a.views < b.views ? 1 : -1
+  })
+  const dispatch = useDispatch()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    dispatch(addBird(birdName))
+    setBird('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Bird List</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Add Bird</p>
+          <input
+            type='text'
+            onChange={(e) => setBird(e.target.value)}
+            value={birdName}
+          />
+        </label>
+        <div>
+          <button type='submit'>Add</button>
+        </div>
+      </form>
+
+      <ul>
+        {birds.map((bird) => (
+          <li key={bird.name}>
+            <h3>{bird.name}</h3>
+            <div>
+              Views: {bird.views}
+              <button onClick={() => dispatch(incrementBird(bird.name))}>
+                <span role='img' aria-label='add'>
+                  ➕
+                </span>
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
